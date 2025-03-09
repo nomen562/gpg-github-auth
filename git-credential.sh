2b8dbf08d9f127412eba81544381f1e327254b0d
malik <malikrishit0522@gmail.com>
2025-03-09 17:48:36 +0530

    Update every step in text

--- git-credential.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#--- This does not work on non git repo directories ---#
-
-# Generate a key first
-sudo chmod u+x /usr/share/doc/git/contrib/credential/netrc/git-credential-netrc.perl
-# Change the path of .netrc.gpg
-git config --global credential.helper /usr/share/doc/git/contrib/credential/netrc/git-credential-netrc.perl
